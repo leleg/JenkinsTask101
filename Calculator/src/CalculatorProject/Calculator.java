51917a20035a16146320a0e6a55843b6f9d39f50
Gaurav Lele <lelegaurav@gmail.com>
2017-01-24 18:35:30 +0530

    Subtraction method

--- Calculator/src/CalculatorProject/Calculator.java
+++ Calculator/src/CalculatorProject/Calculator.java
@@ -55,6 +55,10 @@ public int add(int i, int j)
 }
 public int subtract(int firstOpperand, int secondOpperand) 
 {
+	System.out.println("Enter the first number :");
+	firstOpperand = input.nextInt();
+	System.out.println("Enter the second number :");
+	secondOpperand = input.nextInt();
 	
 	return firstOpperand - secondOpperand;
 
