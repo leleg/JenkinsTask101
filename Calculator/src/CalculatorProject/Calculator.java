2fdb6ffd91eb8b0272f10c41755b48c25045bbb6
Gaurav Lele <lelegaurav@gmail.com>
2017-01-24 19:24:51 +0530

    Multiplication

--- Calculator/src/CalculatorProject/Calculator.java
+++ Calculator/src/CalculatorProject/Calculator.java
@@ -42,30 +42,31 @@ public class Calculator {
 		}
 	}
 
-	
-public int add(int i, int j)
-{
-  System.out.println("Enter the first number :");
+	public int add(int i, int j) {
+		System.out.println("Enter the first number :");
 		firstOpperand = input.nextInt();
 		System.out.println("Enter the second number :");
 		secondOpperand = input.nextInt();
 
 		return firstOpperand + secondOpperand;
-	
-}
 
-public int subtract(int firstOpperand, int secondOpperand) 
-{
-	System.out.println("Enter the first number :");
-	firstOpperand = input.nextInt();
-	System.out.println("Enter the second number :");
-	secondOpperand = input.nextInt();
-	
-	return firstOpperand - secondOpperand;
+	}
+
+	public int subtract(int firstOpperand, int secondOpperand) {
+		System.out.println("Enter the first number :");
+		firstOpperand = input.nextInt();
+		System.out.println("Enter the second number :");
+		secondOpperand = input.nextInt();
+
+		return firstOpperand - secondOpperand;
 
 	}
 
 	public int multiply(int firstOpperand, int secondOpperand) {
+		System.out.println("Enter the first number :");
+		firstOpperand = input.nextInt();
+		System.out.println("Enter the second number :");
+		secondOpperand = input.nextInt();
 
 		return firstOpperand * secondOpperand;
 	}
