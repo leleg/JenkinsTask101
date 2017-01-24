f4d2dc65a3027f061f041c54dbc95dde35cf5d93
Gaurav Lele <lelegaurav@gmail.com>
2017-01-24 12:24:00 +0530

    addition included

--- Calculator/src/CalculatorProject/Calculator.java
+++ Calculator/src/CalculatorProject/Calculator.java
@@ -48,7 +48,10 @@ public void startCalculator(){
 
 public int add(int i, int j)
 {
-	
+	System.out.println("Enter the first number :");
+	firstOpperand = input.nextInt();
+	System.out.println("Enter the second number :");
+	secondOpperand = input.nextInt();
 	return firstOpperand + secondOpperand;
 	
 }
