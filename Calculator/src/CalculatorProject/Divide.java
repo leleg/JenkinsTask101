f0ba855044898bb84668807d1bed910143bcd047
Gaurav Lele <lelegaurav@gmail.com>
2017-01-30 14:55:36 +0530

    Change in Test case

--- Calculator/src/CalculatorProject/Divide.java
+++ Calculator/src/CalculatorProject/Divide.java
@@ -8,7 +8,7 @@ public class Divide {
 	@Test
 	public void test() {
 		Calculator test = new Calculator();
-		int output = test.divide(6,6);
+		int output = test.divide(8,8);
 		assertEquals(1, output);
 	}
 
